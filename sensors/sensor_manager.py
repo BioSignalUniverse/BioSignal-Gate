ffd5cc74a21461affbdfa92ee47e3ef2f26d179a
Jeena Sant Ji <jeenajah@gmail.com>
2026-02-26 16:34:46 -0500

    Update sensor_manager.py

--- sensors/sensor_manager.py
+++ sensors/sensor_manager.py
@@ -1,1 +1,20 @@
 
+# sensors/sensor_manager.py
+
+from sensors.simulation_sensor import SimulationSensor
+
+
+class SensorManager:
+
+    def __init__(self):
+        self.sensor = SimulationSensor()
+
+    async def initialize(self):
+        """
+        Later we can attempt real device connection here.
+        For now, simulation is the default.
+        """
+        pass
+
+    async def measure(self):
+        return await self.sensor.measure()
